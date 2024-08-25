6dc8391bb5bad8aa762015a9d9bc1693709d1d82
SHADECORE <space0amazing@gmail.com>
2024-08-25 14:36:21 +0300

    initial commit

---
+++ src/main/java/net/shadecore/echo/Echo.java
@@ -0,0 +1,15 @@
+package net.shadecore.echo;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Echo implements ModInitializer {
+	public static final String MOD_ID = "echo";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/shadecore/echo/EchoClient.java
@@ -0,0 +1,10 @@
+package net.shadecore.echo;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EchoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
